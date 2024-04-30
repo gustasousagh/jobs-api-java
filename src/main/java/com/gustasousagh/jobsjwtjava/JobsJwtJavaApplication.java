d3f67176e9c3e44c5a2ea22aa98b7bc538275f24
gusta <gustavosousa30246@gmail.com>
2024-04-30 16:42:49 -0300

    init app

---
+++ src/main/java/com/gustasousagh/jobsjwtjava/JobsJwtJavaApplication.java
@@ -0,0 +1,13 @@
+package com.gustasousagh.jobsjwtjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobsJwtJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobsJwtJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gustasousagh/jobsjwtjava/JobsJwtJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustasousagh.jobsjwtjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobsJwtJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
